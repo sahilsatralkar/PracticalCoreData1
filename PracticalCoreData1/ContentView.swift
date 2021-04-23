1445190a752fae514bae139c998f80e562feb83c
Sahil Satralkar <sahil.satralkar@gmail.com>
2021-04-23 21:04:04 +0530

    Initial Commit

---
+++ PracticalCoreData1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticalCoreData1
+//
+//  Created by Sahil Satralkar on 16/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
